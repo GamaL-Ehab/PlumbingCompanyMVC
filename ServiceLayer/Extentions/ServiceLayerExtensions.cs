50b659583702d0c29fac463a68aa8aca080c571e
Gamal-Ehab <gamalehabg@gmail.com>
2025-12-02 03:23:23 +0200

    Registering All Services at The Main Method

--- ServiceLayer/Extentions/ServiceLayerExtensions.cs
+++ ServiceLayer/Extentions/ServiceLayerExtensions.cs
@@ -9,6 +9,17 @@ namespace ServiceLayer.Extentions
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsClass && !x.IsAbstract && x.Name.EndsWith("Service"));
+            foreach (var serviceType in types) 
+            { 
+                var iServiceType = serviceType.GetInterfaces().FirstOrDefault(x => x.Name == $"I{serviceType.Name}");
+
+                if(iServiceType is not null)
+                {
+                    services.AddScoped(iServiceType, serviceType);
+                }
+            }
+
             return services;
         }
     }
