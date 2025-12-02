2aa6cfd83200e34f965473bf3997668284c7310f
Gamal-Ehab <gamalehabg@gmail.com>
2025-12-02 03:12:44 +0200

    Adding Html Views

--- PlumbingStore/Program.cs
+++ PlumbingStore/Program.cs
@@ -31,9 +31,18 @@ namespace PlumbingStore
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
+            #pragma warning disable ASP0014
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                         name: "Admin",
+                         areaName: "Admin",
+                         pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run();
         }
